bc2e26cc2885baad606d8cef0ba3ccf9a9064655
NeoSharp <asmurfette8+2134@gmail.com>
2015-03-21 14:28:42 -0400

    Update General.xaml.cs

--- Views/Settings/General.xaml.cs
+++ Views/Settings/General.xaml.cs
@@ -123,11 +123,10 @@ namespace LeagueSharp.Loader.Views.Settings
             }
         }
             
-        private void Button_Click(object sender, RoutedEventArgs e) {
-            
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
         ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent(accentArray[n]), ThemeManager.GetAppTheme("BaseLight"));
         n = (n + 1) % 23;
-        
         }
 
     }
